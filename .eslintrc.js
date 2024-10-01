9e40426f6e6f9389a81f3a0497688c96a562a90d
Angelo <solonasusonly0@gmail.com>
2024-10-01 15:42:00 -0300

    🔧 chore: add new eslint rule & remove prettier file config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,8 @@ module.exports = {
   extends: [
     '@rocketseat/eslint-config/node',
   ],
-  ignorePatterns: ['.eslintrc.js', 'node_modules'],
+  ignorePatterns: ['.eslintrc.js', 'node_modules', 'data'],
   rules: {
-    // 'no-useless-constructor': true
+    'no-useless-constructor': 'off'
   },
 };
