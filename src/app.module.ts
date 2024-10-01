0b6c93548b5db4d57b1f7871e5dce4853f449dc7
Angelo <solonasusonly0@gmail.com>
2024-10-01 11:17:39 -0300

    ✨ feat: create env module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { envSchema } from './infra/env/env'
+import { EnvService } from './infra/env/env.service'
 
 @Module({
-  imports: [],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      validate: (val) => envSchema.parse(val),
+      isGlobal: true,
+    }),
+  ],
+  providers: [EnvService],
 })
 export class AppModule {}
---
+++ src/infra/env/env.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { ENV } from './env'
+
+@Injectable()
+export class EnvService {
+  constructor(private configService: ConfigService<ENV, true>) {}
+
+  get<T extends keyof ENV>(env: T) {
+    return this.configService.get(env, { infer: true })
+  } 
+}
