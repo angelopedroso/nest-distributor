0fca0665ec9a8d8bd95bad8bc2abd87849a90be1
Angelo <solonasusonly0@gmail.com>
2024-10-01 11:15:11 -0300

    🎉 Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: [
+    '@rocketseat/eslint-config/node',
+  ],
+  ignorePatterns: ['.eslintrc.js', 'node_modules'],
+  rules: {
+    // 'no-useless-constructor': true
+  },
+};
