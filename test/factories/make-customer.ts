76f760ab966f34e97b0e45349e690a2cc5acfeeb
Angelo <solonasusonly0@gmail.com>
2024-10-02 17:44:41 -0300

    ✨ feat: create customer factory

---
+++ test/factories/make-customer.ts
@@ -0,0 +1,24 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import {
+  Customer,
+  CustomerProps,
+} from '@/domain/distribuidora/enterprise/entities/customer'
+import { faker } from '@faker-js/faker/.'
+
+export function MakeCustomer(
+  override: Partial<CustomerProps>,
+  id?: UniqueEntityID,
+) {
+  return Customer.create(
+    {
+      addressId: new UniqueEntityID(),
+      recipientId: new UniqueEntityID(),
+      document: '69119164000139',
+      email: faker.internet.email(),
+      name: faker.person.fullName(),
+      phone: faker.phone.number(),
+      ...override,
+    },
+    id,
+  )
+}
