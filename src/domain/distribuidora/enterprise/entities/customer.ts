0233a1ecca9cb32746a9e3445d4ed06bf5fab482
Angelo <solonasusonly0@gmail.com>
2024-10-02 14:09:45 -0300

    ✨ feat: create customer entity

---
+++ src/domain/distribuidora/enterprise/entities/customer.ts
@@ -0,0 +1,62 @@
+import { Entity } from '@/core/entities/entity'
+import { Optional } from '@/core/types/optional'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+
+export interface CustomerProps {
+  recipientId: UniqueEntityID
+  name: string
+  document: string
+  type: 'fisíca' | 'jurídica'
+  stateRegistration?: string
+  phone: string
+  email: string
+  createdAt: Date
+}
+
+export class Customer extends Entity<CustomerProps> {
+  get recipientId() {
+    return this.props.recipientId
+  }
+
+  get name() {
+    return this.props.name
+  }
+
+  get document() {
+    return this.props.document
+  }
+
+  get type() {
+    return this.props.type
+  }
+
+  get stateRegistration() {
+    return this.props.stateRegistration
+  }
+
+  get phone() {
+    return this.props.phone
+  }
+
+  get email() {
+    return this.props.email
+  }
+
+  static create(
+    props: Optional<CustomerProps, 'type' | 'createdAt'>,
+    id?: UniqueEntityID,
+  ) {
+    const type = props.document.length > 11 ? 'jurídica' : 'fisíca'
+
+    const customer = new Customer(
+      {
+        ...props,
+        createdAt: props.createdAt ?? new Date(),
+        type,
+      },
+      id,
+    )
+
+    return customer
+  }
+}
