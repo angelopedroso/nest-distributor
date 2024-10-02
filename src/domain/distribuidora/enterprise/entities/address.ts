2f934c2aa798d0205af51d9caf025789b48c9bd4
Angelo <solonasusonly0@gmail.com>
2024-10-02 14:10:18 -0300

    ✨feat: create address entity

---
+++ src/domain/distribuidora/enterprise/entities/address.ts
@@ -0,0 +1,42 @@
+import { Entity } from '@/core/entities/entity'
+import { BadRequestError } from '@/core/errors/errors/bad-request-error'
+
+export interface AddressProps {
+  neighborhood: string
+  uf: string
+  city: string
+  street: string
+  cep: string
+}
+
+export abstract class Address<
+  Props extends AddressProps,
+> extends Entity<Props> {
+  get neighborhood() {
+    return this.props.neighborhood
+  }
+
+  get street() {
+    return this.props.street
+  }
+
+  get uf() {
+    return this.props.uf
+  }
+
+  set uf(uf: string) {
+    if (uf.length !== 2) {
+      throw new BadRequestError('UF')
+    }
+
+    this.props.uf = uf.toUpperCase()
+  }
+
+  get city() {
+    return this.props.city
+  }
+
+  get cep() {
+    return this.props.cep
+  }
+}
