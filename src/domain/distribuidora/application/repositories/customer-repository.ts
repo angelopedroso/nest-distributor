a0c5b9a29f32785d11f655835cbd4b33f4c744b2
Angelo <solonasusonly0@gmail.com>
2024-10-02 16:35:19 -0300

    ✨ feat: add delete method in customer repository

--- src/domain/distribuidora/application/repositories/customer-repository.ts
+++ src/domain/distribuidora/application/repositories/customer-repository.ts
@@ -1,14 +1,17 @@
 import { PaginationParams } from '@/core/repositories/pagination-params'
 import { Customer } from '../../enterprise/entities/customer'
+import { CustomerWithAddress } from '../../enterprise/entities/value-objects/customer-with-address'
 
 export abstract class CustomerRepository {
   abstract create(customer: Customer): Promise<void>
   abstract save(customer: Customer): Promise<void>
+  abstract delete(customer: Customer): Promise<void>
 
+  abstract findById(id: string): Promise<Customer | null>
   abstract findManyByRecipientIdWithAddress(
     recipientId: string,
     params: PaginationParams,
-  ): Promise<Customer[]>
+  ): Promise<CustomerWithAddress[]>
 
-  abstract findByIdWithAddress(id: string): Promise<Customer>
+  abstract findByIdWithAddress(id: string): Promise<CustomerWithAddress | null>
 }
